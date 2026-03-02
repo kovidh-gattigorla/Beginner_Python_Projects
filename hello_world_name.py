37b3616d722d73c9c4653e7776872a6c5af60206
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2026-03-02 17:01:12 +0530

    Add files via upload

---
+++ hello_world_name.py
@@ -0,0 +1,7 @@
+# For making the hello_world with users input
+# First we need to given the input 
+name = input('Enter the name :')                                          #For the input
+# Now inputing + printing hello
+print('Hello',name)
+print('Hello',name,'Glad to meet you')                                    #Giving the text After the input
+print(f'Hello {name} Thankyou for giving')                                #Type 2 Need of f in it for the middle mention with {}
