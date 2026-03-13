569d60ee148ac68f75c4db8b4f8b6f5f74c383a6
Kovidh_Gattigorla <134358042+kovidh-gattigorla@users.noreply.github.com>
2026-03-13 22:02:47 +0530

    Add files via upload

--- factorial_calculator.py
+++ factorial_calculator.py
@@ -1,6 +1,8 @@
-num = int(input('Enter the Number: '))
-factorial = 1
-for i in range (1,num+1):
-    factorial = factorial*i
-
+# To make the factorial calculator we need input from user
+num = int(input('Enter the Number: '))                                   # Input from user
+# Taking some value fo factorial
+factorial = 1                                                            
+# Using for loop making the count of every element
+for i in range (1,num+1):                                                # For i in range from 1 to the input number from user
+    factorial = factorial*i                                              # Saving the factorial as 1*i(here i is in loop so it will be counting every element)
 print('Factorial of', num , 'is', factorial)
